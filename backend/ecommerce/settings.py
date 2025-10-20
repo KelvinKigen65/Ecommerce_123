9e681fb5872fa4837271cbd36dc0c5698761d6fe
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-10-20 13:47:07 +0300

    updated orders

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -132,8 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://localhost:5173",
+    "http://127.0.0.1:8000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
