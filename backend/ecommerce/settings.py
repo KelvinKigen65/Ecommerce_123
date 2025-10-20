3c4609134b2e2d63c73e0103f74b4371f0c602bd
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-10-20 23:47:49 +0300

    Allowed cors by adding localhost:5173

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -132,8 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:5173",
-    "http://127.0.0.1:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
