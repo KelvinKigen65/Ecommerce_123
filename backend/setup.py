231181f0f21e09789a709b77ba9339a805d70888
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-10-22 14:54:19 +0300

    Set Python runtime to 3.12.6

--- backend/setup.py
+++
@@ -1,9 +0,0 @@
-# Instead of complex version detection, use a simple approach:
-from setuptools import setup, find_packages
-
-setup(
-    name="ecommerce",
-    version="0.1.0",  # Set version directly
-    packages=find_packages(),
-    # ... other setup arguments
-)
