073a14caa8990f39aeb5570a6a8bc17267cfb310
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-10-24 01:58:24 +0300

    added frontend domain to CORS_ALLOWED_HOST

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "ecommerce-123.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
