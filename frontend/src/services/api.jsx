3a2d1e5ae7d6cbe16497212f07a31763eb7f5df5
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-10-24 11:35:03 +0300

    updated frontend/.env

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -6,7 +6,8 @@
 import axios from 'axios';
 
 // Change this if your Django server runs on another port
-const API_BASE_URL = 'https://ecommerce-123-ouy4.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
